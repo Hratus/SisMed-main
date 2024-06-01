200d5219e9417c1f3270f63831d31d5f237f9e8b
HRATUS <rhatuz29@gmail.com>
2024-06-01 10:29:12 -0300

    Atualizei os packages e realizei a coneção com o parse server

--- SisMed-main/sis_med/lib/main.dart
+++ SisMed-main/sis_med/lib/main.dart
@@ -1,11 +1,29 @@
 import 'package:flutter/material.dart';
 import 'pages/PrincipalPage.dart'; // Certifique-se de que o caminho esteja correto
+import 'package:parse_server_sdk_flutter/parse_server_sdk_flutter.dart';
 
-void main() {
+
+void main() async{
   runApp(MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+       final keyApplicationId = 'E6HNaApKj5z8wWPcdkurSNblLJHGRiR3ojGj3Shl';
+       final keyClientKey = 'dZmKErTwnzs3EqGw1NaQPNoZcvAsIG4G4RQh0Zx3';
+       final keyParseServerUrl = 'https://parseapi.back4app.com';
+
+       await Parse().initialize(keyApplicationId, keyParseServerUrl,
+               clientKey: keyClientKey, autoSendSessionId: true);
+  //Inicia uma classe no parse_server
+  //var firstObject = ParseObject('FirstClass')
+  //     ..set(
+  //              'message', 'Hey ! First message from Flutter. Parse is now connected');
+   // await firstObject.save();
+   // print('done');
+
 }
 
 class MyApp extends StatelessWidget {
+ 
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
