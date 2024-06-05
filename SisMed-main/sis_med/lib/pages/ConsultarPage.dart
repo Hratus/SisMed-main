759a352c71d1a49344353d6a00305c062119a544
GILDO ADRIANO NORBERTO DA SILVA <133284362+GildoAdrianoZERO@users.noreply.github.com>
2024-06-05 15:55:32 -0300

    alterações na ConsultarPage

--- SisMed-main/sis_med/lib/pages/ConsultarPage.dart
+++ SisMed-main/sis_med/lib/pages/ConsultarPage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:parse_server_sdk_flutter/parse_server_sdk_flutter.dart';
+import 'HomePage.dart';
 
 class ConsultarPage extends StatefulWidget {
   @override
@@ -14,7 +15,6 @@ class _ConsultarPageState extends State<ConsultarPage> {
   void initState() {
     super.initState();
     _fetchAgendamentos();
-
   }
 
   Future<void> _fetchAgendamentos() async {
@@ -29,45 +29,108 @@ class _ConsultarPageState extends State<ConsultarPage> {
     }
   }
 
+  void _cancelarAgendamento(ParseObject agendamento) {
+    // Implemente aqui a lógica para cancelar o agendamento
+    // Isso pode envolver o envio de uma solicitação ao servidor para cancelar o agendamento
+    // e atualizar a lista de agendamentos após o cancelamento
+  }
 
-
-
-
-
-
+  void _voltarHomePage() {
+    Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => HomePage()));
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Consultar Agendamentos'),
-      ),
-      body: ListView.builder(
-        itemCount: agendamentos.length,
-        itemBuilder: (context, index) {
-          final agendamento = agendamentos[index];
-          final especialidade = agendamento.get<String>('especialidade') ?? '';
-          final modalidade = agendamento.get<String>('modalidade') ?? '';
-          final profissional = agendamento.get<String>('medico') ?? '';
-          final data = agendamento.get<String>('data') ?? '';
-
-
-          return ListTile(
-            title: Text('Especialidade: $especialidade'),
-            subtitle: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text('Modalidade: $modalidade'),
-                Text('Medico: $profissional'),
-                Text('Data: $data'),
-              ],
+      backgroundColor: Color(0xFFB9CCF2),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Lista de Agendamentos',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
             ),
-          );
-        },
+            SizedBox(height: 20),
+            Expanded(
+              child: ListView.builder(
+                itemCount: agendamentos.length,
+                itemBuilder: (context, index) {
+                  final agendamento = agendamentos[index];
+                  final especialidade = agendamento.get<String>('especialidade') ?? '';
+                  final modalidade = agendamento.get<String>('modalidade') ?? '';
+                  final profissional = agendamento.get<String>('medico') ?? '';
+                  final data = agendamento.get<String>('data') ?? '';
+
+                  return GestureDetector(
+                    onTap: () {
+                      showModalBottomSheet(
+                        context: context,
+                        builder: (context) {
+                          return Container(
+                            child: Column(
+                              mainAxisSize: MainAxisSize.min,
+                              children: <Widget>[
+                                ListTile(
+                                  leading: Icon(Icons.cancel),
+                                  title: Text('Cancelar Agendamento'),
+                                  onTap: () {
+                                    _cancelarAgendamento(agendamento);
+                                    Navigator.pop(context);
+                                  },
+                                ),
+                                ListTile(
+                                  leading: Icon(Icons.arrow_back),
+                                  title: Text('Voltar para a Homepage'),
+                                  onTap: () {
+                                    _voltarHomePage();
+                                  },
+                                ),
+                              ],
+                            ),
+                          );
+                        },
+                      );
+                    },
+                    child: Container(
+                      margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                      padding: EdgeInsets.all(20),
+                      decoration: BoxDecoration(
+                        color: Colors.white, // Cor de fundo dos blocos
+                        borderRadius: BorderRadius.circular(10), // Borda arredondada
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Especialidade: $especialidade',
+                            style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+                          ),
+                          SizedBox(height: 10),
+                          Text(
+                            'Modalidade: $modalidade',
+                            style: TextStyle(color: Colors.black),
+                          ),
+                          SizedBox(height: 10),
+                          Text(
+                            'Medico: $profissional',
+                            style: TextStyle(color: Colors.black),
+                          ),
+                          SizedBox(height: 10),
+                          Text(
+                            'Data: $data',
+                            style: TextStyle(color: Colors.black),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
-
-
 }
-
